0c42d5ce6e8e556f41dde27137b407e3c4799b19
Yosselyn Campos <yossealexandra@gmail.com>
2024-04-27 10:44:59 -0400

    Se agrega implementación openapi Swagger

--- src/main/java/com/projectbase2/exam/student/config/OpenApiConfig.java
+++ src/main/java/com/projectbase2/exam/student/config/OpenApiConfig.java
@@ -1,2 +1,20 @@
-package com.projectbase2.exam.student.config;public class OpenApiConfig {
+package com.projectbase2.exam.student.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title="Exam Project Base2",
+                version="0.0.1",
+                description = "Proyecto base Examen Base2",
+                termsOfService = "http://swagger.io/terms/"
+        )
+)
+public class OpenApiConfig {
+
 }
